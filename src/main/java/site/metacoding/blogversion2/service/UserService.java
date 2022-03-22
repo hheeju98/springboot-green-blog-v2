4f04db9c34f0abd1b8e84e9204ea48853c3db553
hheeju98 <hheeju98@naver.com>
2022-03-23 00:06:26 +0900

    5. 회원가입 완료

--- src/main/java/site/metacoding/blogversion2/service/UserService.java
+++ src/main/java/site/metacoding/blogversion2/service/UserService.java
@@ -1,5 +1,7 @@
 package site.metacoding.blogversion2.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -9,4 +11,10 @@ import site.metacoding.blogversion2.domain.user.UserRepository;
 @Service // 컴포넌트 스캔시에 IoC 컨테이너에 등록됨 // 트랜잭션 관리하는 오브젝트임. 기능 모임
 public class UserService {
     private final UserRepository userRepository;
+
+    @Transactional
+    public void 회원가입 (JoinDto, joinDto){
+        // save하면 db에 insert하고 insert된 결과를 다시 return 해준다.
+        userRepository.save(joinDto.toEntity());
+    }
 }
