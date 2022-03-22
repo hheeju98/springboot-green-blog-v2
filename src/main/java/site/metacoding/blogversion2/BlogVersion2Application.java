a670b2767c86f01a14bf186d21cd84039f6dac39
hheeju98 <hheeju98@naver.com>
2022-03-22 23:08:05 +0900

    1. 프로젝트 세팅 VIEW 완료

---
+++ src/main/java/site/metacoding/blogversion2/BlogVersion2Application.java
@@ -0,0 +1,13 @@
+package site.metacoding.blogversion2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogVersion2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogVersion2Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/blogversion2/BlogVersion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.blogversion2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogVersion2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
