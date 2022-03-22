404c1136cc4cb9445ecf60b59be42c22ac296ef9
hheeju98 <hheeju98@naver.com>
2022-03-23 00:12:38 +0900

    6. 글로벌 익셉션 처리하는 완료

--- src/main/java/site/metacoding/blogversion2/service/UserService.java
+++ src/main/java/site/metacoding/blogversion2/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogversion2.domain.user.UserRepository;
+import site.metacoding.blogversion2.web.api.dto.user.JoinDto;
 
 @RequiredArgsConstructor
 @Service // 컴포넌트 스캔시에 IoC 컨테이너에 등록됨 // 트랜잭션 관리하는 오브젝트임. 기능 모임
@@ -13,8 +14,10 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public void 회원가입 (JoinDto, joinDto){
+    public void 회원가입(JoinDto joinDto) {
         // save하면 db에 insert하고 insert된 결과를 다시 return 해준다.
+
         userRepository.save(joinDto.toEntity());
+
     }
 }
--- src/main/java/site/metacoding/blogversion2/web/api/UserApiController.java
+++ src/main/java/site/metacoding/blogversion2/web/api/UserApiController.java
@@ -1,12 +1,23 @@
 package site.metacoding.blogversion2.web.api;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogversion2.service.UserService;
+import site.metacoding.blogversion2.web.api.dto.ResponseDto;
+import site.metacoding.blogversion2.web.api.dto.user.JoinDto;
 
 @RequiredArgsConstructor
 @RestController
 public class UserApiController {
     private final UserService userService;
+
+    @PostMapping("/api/join")
+    public ResponseDto<String> join(@RequestBody JoinDto joinDto) {
+        userService.회원가입(joinDto);
+
+        return new ResponseDto<String>(1, "회원가입성공", null);
+    }
 }
